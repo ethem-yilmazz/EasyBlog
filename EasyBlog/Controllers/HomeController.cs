37532143309b7487b0a11b1315a3427e532a1743
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-02-08 11:52:03 +0300

    show post, filter category

--- EasyBlog/Controllers/HomeController.cs
+++ EasyBlog/Controllers/HomeController.cs
@@ -1,35 +1,53 @@
 using EasyBlog.Data;
 using EasyBlog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EasyBlog.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 		private readonly ApplicationDbContext _db;
 
-		public HomeController(ILogger<HomeController> logger,ApplicationDbContext db)
-        {
-            _logger = logger;
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+		{
+			_logger = logger;
 			_db = db;
 		}
 
-        public IActionResult Index()
-        {
-            return View(_db.Posts.OrderByDescending(x=> x.Id).ToList());
-        }
+		public IActionResult Index(int cid)
+		{
+			if(cid != 0)
+			{
+				var category = _db.Categories.FirstOrDefault(x => x.Id == cid);
+				if(category == null)
+					return NotFound();
+				ViewBag.CategoryName = category.Name;
+				return View(_db.Posts.Where(x => x.CategoryId == cid).OrderByDescending(x => x.Id).ToList());
+			}
+			return View(_db.Posts.OrderByDescending(x => x.Id).ToList());
+		}
+		
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[Route("Post/{id}")]
+		public IActionResult ShowPost(int id)
+		{
+			var post = _db.Posts.Include(x => x.Category).FirstOrDefault(x => x.Id == id);
+
+			return View(post);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
